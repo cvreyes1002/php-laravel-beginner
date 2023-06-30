f26cd4258d777c2a55d522e19b9dfdc4d1e48e5f
C.Reyes <cvreyes@hotmail.com>
2023-06-30 07:57:07 +0000

    4.Auth Basics-1.LoggingIn-50%

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed']
+        ]);
+        
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+
+        User::create($incomingFields);
+        return 'Hello from register function.';
+    }
+
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+            return 'Success!';
+        } else {
+            return 'Failed!';
+        }
+    }
+}
